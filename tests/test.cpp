3d1715511d9169d79e707a2e22abe7de137975e2
Harsh M <harshmaroo123@gmail.com>
2021-03-25 15:05:26 +0530

    1. Adds the properties for testing the getDouble function 2. Updates test.cpp to test getDouble

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,6 @@
 #include "../include/GSS/Reader.hpp"
 #include <cassert>
+#include <cmath>
 
 int main()
 {
@@ -7,5 +8,13 @@ int main()
     assert(source.getString("MainToolbar::DropdownMenu::Button::background-color") == "#ffffff");
     assert(source.getInt("MainToolbar::Button::width") == 50);
     assert(source.getInt("MainToolbar::Button::height") == 20);
+
+    //Testing for getDouble
+    GSS::PropertySheet rpgData = GSS::PropertySheet::loadFromFile("properties.gss");
+    double life = rpgData.getDouble("RPG::Hero::life");
+    double attack = rpgData.getDouble("RPG::Hero::attack");
+    std::cout << life << std::endl;       // Outputs 42,5.
+    std::cout << ceil(life) << std::endl; // Outputs 43,0.
+
     return 0;
 }
