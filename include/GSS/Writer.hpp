e0e4f5bd001b983f014f255551e9449734fac143
GaryNLOL <garypastel@gmail.com>
2021-03-26 10:53:03 -0500

    Added #pragma once to Reader and created Writer file

---
+++ include/GSS/Writer.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace GSS
+{
+    class Writer
+    {
+
+    };
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,17 +4,19 @@
 
 int main()
 {
+    //Testing for loadFromFile
     GSS::PropertySheet source = GSS::PropertySheet::loadFromFile("properties.gss");
-    assert(source.getString("MainToolbar::DropdownMenu::Button::background-color") == "#ffffff");
+
+    //Testing for getString
+    assert(source.getString("MainToolbar::Button::background-color") == "#000000");
+    assert(source.getString("MainToolbar::Button::color") == "#ffffff");
+
+    //Testing for getInt
     assert(source.getInt("MainToolbar::Button::width") == 50);
     assert(source.getInt("MainToolbar::Button::height") == 20);
 
     //Testing for getDouble
-    GSS::PropertySheet rpgData = GSS::PropertySheet::loadFromFile("properties.gss");
-    double life = rpgData.getDouble("RPG::Hero::life");
-    double attack = rpgData.getDouble("RPG::Hero::attack");
-    std::cout << life << std::endl;       // Outputs 42,5.
-    std::cout << ceil(life) << std::endl; // Outputs 43,0.
-
-    return 0;
+    assert(source.getDouble("RPG::Hero::attack") == 38.0);
+    assert(source.getDouble("RPG::Hero::life") == 42.5);
+    assert(ceil(source.getDouble("RPG::Hero::attack")) == 43.0);
 }
